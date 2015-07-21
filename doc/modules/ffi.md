2095289dc784e5de4e92a762c5f94f38247f0d1a
Kai Böse <kai.boese@web.de>
2015-07-21 21:16:52 +0200

    Reworked complete FFI Module and fixed loader order for dependencies.

--- doc/modules/ffi.md
+++ doc/modules/ffi.md
@@ -5,54 +5,144 @@ FFI module for C-Library-Calls
 * Shortname: FFI
 * Version: 1.0
 * Author: Kagu-chan
-* Depends on: [Config](../modules/config.md), [Cache](../modules/cache.md)
+* Depends on: [Cache](../modules/cache.md), [OOP](../modules/oop.md), [Table](../modules/table.md)
 
-> This Module provides the FFI library and some short cuts. Also it provides the Advapi32.dll and libpng.dll
+> This Module provides the FFI library and some short cuts.
 
-###Spindle.ffi.user_init
-Optional Callback for initialization extension
+###FFIObject
+FFIObject Object
+
+###FFIObject.new(string name, string cdef, function init_callback, table functions, string load_library)
+Creates a new Instance of FFIObject
 ```lua
-Spindle.ffi.user_init = function() Spindle.debug("FFI initialized") end
+local ffi_object = FFIObject.new("example", [[typedef unsigned int UINT;]], function() end, {}, ""
 ```
 
-###Spindle.ffi.no_windows_callback
-Optional Callback for non windows initialization
+###FFIObject.fromtable(table t)
+Creates instance from given table
 ```lua
-Spindle.ffi.no_windows_callback = function() Spindle.debug("Add Unix initialization here") end
+local t = {name = "example", cdef = [[typedef unsigned int UINT;]], init_callback = function() end, functions = {}, load_library = ""}
+local ffi_object = FFIObject.fromtable(t
+```
+
+###FFIObject:name([string name])
+Sets or gets name value of instance
+```lua
+local ffi_object = FFIObject.new("example", [[typedef unsigned int UINT;]], function() end, {}, "")
+ffi_object:name("test")
+ffi_object:name() -- "test
+```
+
+###FFIObject:cdef([string cdef])
+Sets or gets cdef value of instance
+```lua
+local ffi_object = FFIObject.new("example", [[typedef unsigned int UINT;]], function() end, {}, "")
+ffi_object:cdef([[typedef int INT;]])
+ffi_object:cdef() -- [[typedef int INT;]
+```
+
+###FFIObject:init_callback([function init_callback])
+Sets or gets init_callback value of instance
+```lua
+local ffi_object = FFIObject.new("example", [[typedef unsigned int UINT;]], function() end, {}, "")
+ffi_object:init_callback(function() print(self:name()) end)
+ffi_object:init_callback() -- fun
+```
+
+###FFIObject:functions([table functions])
+Sets or gets functions value of instance
+```lua
+local ffi_object = FFIObject.new("example", [[typedef unsigned int UINT;]], function() end, {}, "")
+ffi_object:functions({test = function() return 0 end})
+ffi_object:functions() -- {test = func
+```
+
+###FFIObject:load_library([string load_library])
+Sets or gets load_library value of instance
+```lua
+local ffi_object = FFIObject.new("example", [[typedef unsigned int UINT;]], function() end, {}, "")
+ffi_object:load_library("advapi")
+ffi_object:load_library() -- "advapi
+```
+
+###FFIObject:lib([cdata lib])
+Sets or gets lib value of instance
+```lua
+local ffi_object = FFIObject.new("example", [[typedef unsigned int UINT;]], function() end, {}, "")
+ffi_object:lib(Spindle.ffi.load("advapi"))
+ffi_object:lib() -- cdata objec
+```
+
+###FFIObject:initialized([boolean initialized])
+Sets or gets initialized value of instance
+```lua
+local ffi_object = FFIObject.new("example", [[typedef unsigned int UINT;]], function() end, {}, "")
+ffi_object:initialized(true)
+ffi_object:initialized() -- tru
+```
+
+###FFIObject:totable()
+Returns table representation of instance
+```lua
+local ffi_object = FFIObject.new("example", [[typedef unsigned int UINT;]], function() end, {}, "")
+local t = ffi_object:totable() -- {name = "example", cdef = [[typedef unsigned int UINT;]], init_callback = function() end, functions = {}, load_library = ""
+```
+
+###Spindle.ffi.new(string name, string cdef, function init_callback, table functions[, string load_library])
+Add new library with CDEF to ffi module
+```lua
+Spindle.ffi.new(
+	"example",
+	[[
+typedef unsigned int UINT;
+	]],
+	function() end,
+	{
+		func = function(v)
+			return v
+		end,
+	}
+
+```
+
+###Spindle.ffi.send(string ffi_name, string function_name[, ...])
+Sends a method to libraries function table and ensure initialization of library. Tupel represents the parameters for this function-
+```lua
+local utf16_string = Spindle.ffi.send("utf8_ffi", "utf8_to_utf16", "Hello World")
+-- Otherwhise (Example from FFI.UTF8_FFI Module):
+Spindle.ffi.initialize() -- send method ensure initialization of all library definitions. Here you have to do it manually!
+local utf16_string = Spindle.cache.get("ffi_cache").utf8_ffi:functions()["utf8_to_utf16"]("Hello World"
 ```
 
 ###Spindle.ffi.initialize()
 FFI initialization function
-
-###Spindle.ffi.load(...)
-Wrapper for Spindle.ffi.ffi().load(...)
 ```lua
-local advapi = Spindle.ffi.ffi().load("Advapi32")
-```
-
-###Spindle.ffi.cdef(...)
-Wrapper for Spindle.ffi.ffi().cdef(...)
-```lua
-Spindle.ffi.cdef(Spindle.ffi.defines.misc.libpng)
+Spindle.ffi.initialize(
 ```
 
 ###Spindle.ffi.ffi()
-Wrapper for Spindle.cache.get("ffi")
+Wrapper for 'require("ffi")`
 ```lua
-local advapi = Spindle.ffi.ffi().load("Advapi32")
+local cp_utf = Spindle.ffi.ffi().C.CP_UTF8 -- Normally 65001 for utf8 functions - Must be defined via CDEF befor
 ```
 
-###Spindle.ffi.advapi()
-Wrapper for Spindle.cache.get("advapi")
+###Spindle.ffi.load(...)
+Wrapper for 'Spindle.ffi.ffi().load(...)`
 ```lua
-local advapi = Spindle.ffi.advapi() -- Advapi32 must be loaded before
+local advapi = Spindle.ffi.ffi().load("Advapi32"
 ```
 
-###Spindle.ffi.libpng()
-Wrapper for Spindle.cache.get("libpng")
+###Spindle.ffi.cdef(...)
+Wrapper for 'Spindle.ffi.ffi().cdef(...)`
 ```lua
-local libpng = Spindle.ffi.libpng() -- libpng must be loaded before
+Spindle.ffi.cdef([[
+enum{CP_UTF8 = 65001};
+]]
 ```
 
-###Spindle.ffi.defines
-Table with CDEF definitions
+###Spindle.ffi.cache([table cache])
+Sets or gets ffi_cache table in Spindle.cache
+```lua
+local ffi_cache = Spindle.ffi.cache() -- Getter
+Spindle.ffi.cache(ffi_cache) -- Sette
+```
--- src/aegi-spindle-ffi.lua
+++ src/aegi-spindle-ffi.lua
@@ -2,62 +2,88 @@
 name:FFI
 fullname:Spindle.FFI
 description:FFI module for C-Library-Calls
-fulldescription:This Module provides the FFI library and some short cuts. Also it provides the Advapi32.dll and libpng.dll
+fulldescription:This Module provides the FFI library and some short cuts.
 extends:
-depends:Config,Cache
+depends:Cache,OOP,Table
 author:Kagu-chan
 version:1.0
 type:module
 docExternal:https://github.com/Kagurame/AegiSpindle/tree/master/doc/modules/ffi.md
 docInternal:
-	Spindle.ffi.user_init Optional Callback for initialization extension
-	Spindle.ffi.no_windows_callback Optional Callback for non windows initialization
+	FFIObject FFIObject Object
+	FFIObject.new(string name, string cdef, function init_callback, table functions, string load_library) Creates a new Instance of FFIObject
+	FFIObject.fromtable(table t) Creates instance from given table
+	FFIObject:name([string name]) Sets or gets name value of instance
+	FFIObject:cdef([string cdef]) Sets or gets cdef value of instance
+	FFIObject:init_callback([function init_callback]) Sets or gets init_callback value of instance
+	FFIObject:functions([table functions]) Sets or gets functions value of instance
+	FFIObject:load_library([string load_library]) Sets or gets load_library value of instance
+	FFIObject:lib([cdata lib]) Sets or gets lib value of instance
+	FFIObject:initialized([boolean initialized]) Sets or gets initialized value of instance
+	FFIObject:totable() Returns table representation of instance
+	Spindle.ffi.new(string name, string cdef, function init_callback, table functions[, string load_library]) Add new library with CDEF to ffi module
+	Spindle.ffi.send(string ffi_name, string function_name[, ...]) Sends a method to libraries function table and ensure initialization of library. Tupel represents the parameters for this function-
 	Spindle.ffi.initialize() FFI initialization function
-	Spindle.ffi.load(...) Wrapper for Spindle.ffi.ffi().load(...)
-	Spindle.ffi.cdef(...) Wrapper for Spindle.ffi.ffi().cdef(...)
-	Spindle.ffi.ffi() Wrapper for Spindle.cache.get("ffi")
-	Spindle.ffi.advapi() Wrapper for Spindle.cache.get("advapi")
-	Spindle.ffi.libpng() Wrapper for Spindle.cache.get("libpng")
-	Spindle.ffi.defines Table with CDEF definitions
+	Spindle.ffi.ffi() Wrapper for 'require("ffi")`
+	Spindle.ffi.load(...) Wrapper for 'Spindle.ffi.ffi().load(...)`
+	Spindle.ffi.cdef(...) Wrapper for 'Spindle.ffi.ffi().cdef(...)`
+	Spindle.ffi.cache([table cache]) Sets or gets ffi_cache table in Spindle.cache
 ]]
 
 -- Set global functions and objects to local cache for performance
 local Spindle = _G.Spindle or {}
-local require, type = _G.require, _G.type
+local require, type, pairs, pcall = _G.require, _G.type, _G.pairs, _G.pcall
 
-Spindle.modules.require("config")
 Spindle.modules.require("cache")
+Spindle.modules.require("oop")
+Spindle.modules.require("table")
+
+Spindle.oop.generateClass("FFIObject", {
+	initialized = false,
+	lib = {type = "cdata"},
+}, {}, {
+	name = "string",
+	cdef = "string",
+	init_callback = "function",
+	functions = "table",
+	load_library = "string"
+}, "name", "cdef", "init_callback", "functions", "load_library")
 
 Spindle.ffi = {
-	-- Overwrite this with a function to do additional stuff after initialization
-	user_init = false,
-	-- Overwrite this with a function to do additional stuff for non windows systems
-	no_windows_callback = false,
+	new = function(name, cdef, init_callback, functions, load_library)
+		Spindle.assertOverrides(
+			{"string", "string", "function", "table", "string"},
+			{"string", "string", "function", "table"},
+			{name, cdef, init_callback, functions, load_library}
+		)
+		local object = FFIObject.new(name, cdef, init_callback, functions, load_library or "")
+		
+		object:initialized(false)
+		local ffi_cache = Spindle.ffi.cache()
+		ffi_cache[name] = object
+		Spindle.ffi.cache(ffi_cache)
+	end,
+	send = function(ffi_name, function_name, ...)
+		Spindle.ffi.initialize()
+		local object = Spindle.ffi.cache()[ffi_name]
+		local functions = object:functions()
+		return functions[function_name](...)
+	end,
 	initialize = function()
-		if not Spindle.cache.get("ffi_init") then
-			Spindle.cache.set("advapi", false)
-			Spindle.cache.set("libpng", false)
-			Spindle.cache.set("ffi", require("ffi"))
-			
-			if Spindle.ffi.ffi().os == "Windows" then
-				Spindle.cache.set("advapi", Spindle.ffi.ffi().load("Advapi32"))
-				Spindle.ffi.cdef(Spindle.ffi.defines.windows.advapi)
-			else
-				if type(Spindle.ffi.no_windows_callback) == "function" then
-					Spindle.ffi.no_windows_callback()
-				else
-					Spindle.debug("Non-Windows-Systems not supported yet!")
-				end
+		local cache = Spindle.ffi.cache()
+		for name, object in pairs(Spindle.table.select(cache, function(e) return not e:initialized() end)) do
+			Spindle.ffi.cdef(object:cdef())
+			if not object:load_library() == "" then
+				pcall(function() Spindle.ffi.load(object:load_library()) end)
 			end
-			
-			pcall(function()
-				Spindle.cache.set("libpng", Spindle.ffi.load(Spindle.config.getOrDefault("LibPNGPath", "libpng")))
-				Spindle.ffi.cdef(Spindle.ffi.defines.misc.libpng)
-			end)
-			
-			if type(Spindle.ffi.user_init) == "function" then Spindle.ffi.user_init() end
-			Spindle.cache.set("ffi_init", true)
+			object:init_callback()
+			object:initialized(true)
+			cache[name] = object
 		end
+		Spindle.ffi.cache(cache)
+	end,
+	ffi = function()
+		return require("ffi")
 	end,
 	load = function(...)
 		return Spindle.ffi.ffi().load(...)
@@ -65,198 +91,11 @@ Spindle.ffi = {
 	cdef = function(...)
 		return Spindle.ffi.ffi().cdef(...)
 	end,
-	ffi = function()
-		return Spindle.cache.get("ffi")
+	cache = function(cache)
+		if cache then
+			Spindle.cache.set("ffi_cache", cache)
+		else
+			return Spindle.cache.getOrDefault("ffi_cache", {})
+		end
 	end,
-	advapi = function() return Spindle.cache.get("advapi") end,
-	libpng = function() return Spindle.cache.get("libpng") end,
-	defines = {
-		misc = {
-			libpng = [[
-static const int PNG_SIGNATURE_SIZE = 8;
-typedef unsigned char png_byte;
-typedef png_byte* png_bytep;
-typedef const png_bytep png_const_bytep;
-typedef unsigned int png_size_t;
-typedef char png_char;
-typedef png_char* png_charp;
-typedef const png_charp png_const_charp;
-typedef void png_void;
-typedef png_void* png_voidp;
-typedef struct png_struct* png_structp;
-typedef const png_structp png_const_structp;
-typedef struct png_info* png_infop;
-typedef const png_infop png_const_infop;
-typedef unsigned int png_uint_32;
-typedef void (__cdecl *png_error_ptr)(png_structp, png_const_charp);
-typedef void (__cdecl *png_rw_ptr)(png_structp, png_bytep, png_size_t);
-enum{
-	PNG_TRANSFORM_STRIP_16 = 0x1,
-	PNG_TRANSFORM_PACKING = 0x4,
-	PNG_TRANSFORM_EXPAND = 0x10,
-	PNG_TRANSFORM_BGR = 0x80
-};
-enum{
-	PNG_COLOR_MASK_COLOR = 2,
-	PNG_COLOR_MASK_ALPHA = 4
-};
-enum{
-	PNG_COLOR_TYPE_RGB = PNG_COLOR_MASK_COLOR,
-	PNG_COLOR_TYPE_RGBA = PNG_COLOR_MASK_COLOR | PNG_COLOR_MASK_ALPHA
-};
-
-void* memcpy(void*, const void*, size_t);
-int png_sig_cmp(png_const_bytep, png_size_t, png_size_t);
-png_structp png_create_read_struct(png_const_charp, png_voidp, png_error_ptr, png_error_ptr);
-void png_destroy_read_struct(png_structp*, png_infop*, png_infop*);
-png_infop png_create_info_struct(png_structp);
-void png_set_read_fn(png_structp, png_voidp, png_rw_ptr);
-void png_read_png(png_structp, png_infop, int, png_voidp);
-int png_set_interlace_handling(png_structp);
-void png_read_update_info(png_structp, png_infop);
-png_uint_32 png_get_image_width(png_const_structp, png_const_infop);
-png_uint_32 png_get_image_height(png_const_structp, png_const_infop);
-png_byte png_get_color_type(png_const_structp, png_const_infop);
-png_size_t png_get_rowbytes(png_const_structp, png_const_infop);
-png_bytep* png_get_rows(png_const_structp, png_const_infop);
-			]],
-		},
-		windows = {
-			advapi = [[
-enum{CP_UTF8 = 65001};
-enum{MM_TEXT = 1};
-enum{TRANSPARENT = 1};
-enum{
-	FW_NORMAL = 400,
-	FW_BOLD = 700
-};
-enum{DEFAULT_CHARSET = 1};
-enum{OUT_TT_PRECIS = 4};
-enum{CLIP_DEFAULT_PRECIS = 0};
-enum{ANTIALIASED_QUALITY = 4};
-enum{DEFAULT_PITCH = 0x0};
-enum{FF_DONTCARE = 0x0};
-enum{
-	PT_MOVETO = 0x6,
-	PT_LINETO = 0x2,
-	PT_BEZIERTO = 0x4,
-	PT_CLOSEFIGURE = 0x1
-};
-typedef unsigned int UINT;
-typedef unsigned long DWORD;
-typedef DWORD* LPDWORD;
-typedef const char* LPCSTR;
-typedef const wchar_t* LPCWSTR;
-typedef wchar_t* LPWSTR;
-typedef char* LPSTR;
-typedef void* HANDLE;
-typedef HANDLE HDC;
-typedef int BOOL;
-typedef BOOL* LPBOOL;
-typedef unsigned int size_t;
-typedef HANDLE HFONT;
-typedef HANDLE HGDIOBJ;
-typedef long LONG;
-typedef wchar_t WCHAR;
-typedef unsigned char BYTE;
-typedef BYTE* LPBYTE;
-typedef int INT;
-typedef long LPARAM;
-static const int LF_FACESIZE = 32;
-static const int LF_FULLFACESIZE = 64;
-typedef struct{
-	LONG tmHeight;
-	LONG tmAscent;
-	LONG tmDescent;
-	LONG tmInternalLeading;
-	LONG tmExternalLeading;
-	LONG tmAveCharWidth;
-	LONG tmMaxCharWidth;
-	LONG tmWeight;
-	LONG tmOverhang;
-	LONG tmDigitizedAspectX;
-	LONG tmDigitizedAspectY;
-	WCHAR tmFirstChar;
-	WCHAR tmLastChar;
-	WCHAR tmDefaultChar;
-	WCHAR tmBreakChar;
-	BYTE tmItalic;
-	BYTE tmUnderlined;
-	BYTE tmStruckOut;
-	BYTE tmPitchAndFamily;
-	BYTE tmCharSet;
-}TEXTMETRICW, *LPTEXTMETRICW;
-typedef struct{
-	LONG cx;
-	LONG cy;
-}SIZE, *LPSIZE;
-typedef struct{
-	LONG left;
-	LONG top;
-	LONG right;
-	LONG bottom;
-}RECT;
-typedef const RECT* LPCRECT;
-typedef struct{
-	LONG x;
-	LONG y;
-}POINT, *LPPOINT;
-typedef struct{
-  LONG  lfHeight;
-  LONG  lfWidth;
-  LONG  lfEscapement;
-  LONG  lfOrientation;
-  LONG  lfWeight;
-  BYTE  lfItalic;
-  BYTE  lfUnderline;
-  BYTE  lfStrikeOut;
-  BYTE  lfCharSet;
-  BYTE  lfOutPrecision;
-  BYTE  lfClipPrecision;
-  BYTE  lfQuality;
-  BYTE  lfPitchAndFamily;
-  WCHAR lfFaceName[LF_FACESIZE];
-}LOGFONTW, *LPLOGFONTW;
-typedef struct{
-  LOGFONTW elfLogFont;
-  WCHAR   elfFullName[LF_FULLFACESIZE];
-  WCHAR   elfStyle[LF_FACESIZE];
-  WCHAR   elfScript[LF_FACESIZE];
-}ENUMLOGFONTEXW, *LPENUMLOGFONTEXW;
-enum{
-	FONTTYPE_RASTER = 1,
-	FONTTYPE_DEVICE = 2,
-	FONTTYPE_TRUETYPE = 4
-};
-typedef int (__stdcall *FONTENUMPROC)(const ENUMLOGFONTEXW*, const void*, DWORD, LPARAM);
-enum{ERROR_SUCCESS = 0};
-typedef HANDLE HKEY;
-typedef HKEY* PHKEY;
-enum{HKEY_LOCAL_MACHINE = 0x80000002};
-typedef enum{KEY_READ = 0x20019}REGSAM;
-
-int MultiByteToWideChar(UINT, DWORD, LPCSTR, int, LPWSTR, int);
-int WideCharToMultiByte(UINT, DWORD, LPCWSTR, int, LPSTR, int, LPCSTR, LPBOOL);
-HDC CreateCompatibleDC(HDC);
-BOOL DeleteDC(HDC);
-int SetMapMode(HDC, int);
-int SetBkMode(HDC, int);
-size_t wcslen(const wchar_t*);
-HFONT CreateFontW(int, int, int, int, int, DWORD, DWORD, DWORD, DWORD, DWORD, DWORD, DWORD, DWORD, LPCWSTR);
-HGDIOBJ SelectObject(HDC, HGDIOBJ);
-BOOL DeleteObject(HGDIOBJ);
-BOOL GetTextMetricsW(HDC, LPTEXTMETRICW);
-BOOL GetTextExtentPoint32W(HDC, LPCWSTR, int, LPSIZE);
-BOOL BeginPath(HDC);
-BOOL ExtTextOutW(HDC, int, int, UINT, LPCRECT, LPCWSTR, UINT, const INT*);
-BOOL EndPath(HDC);
-int GetPath(HDC, LPPOINT, LPBYTE, int);
-BOOL AbortPath(HDC);
-int EnumFontFamiliesExW(HDC, LPLOGFONTW, FONTENUMPROC, LPARAM, DWORD);
-LONG RegOpenKeyExA(HKEY, LPCSTR, DWORD, REGSAM, PHKEY);
-LONG RegCloseKey(HKEY);
-LONG RegEnumValueW(HKEY, DWORD, LPWSTR, LPDWORD, LPDWORD, LPDWORD, LPBYTE, LPDWORD);
-			]],
-		},
-	},
 }
